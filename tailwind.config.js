c3ff5eb9f5a99e59c10b000d808ffa14cb34f3fa
AnaGCastro <testeanateste0@gmail.com>
2025-05-21 16:12:07 -0300

    Comecando o projeto e criando sidebar

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,32 +1,16 @@
-/** @type {import('tailwindcss').Config} */
+/**  @type {import('tailwindcss').Config} */
 
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: [
+    "./src/**/*.{html,js}",
+  ],
   theme: {
     extend: {
       fontFamily: {
-        "sans": ["Poppins ", "sans-serif"],
+       "sans": ["Poppins"]
       },
-      backgroundImage: {
-        "tenis": "url('./src/assets/images/bg.png')",
-      },
-      keyframes: {
-        floatRight: {
-          "0%": {tranform: "translatey(0px)"},
-          "50%": {tranform: "translatey(8px)"},
-          "100%": {tranform: "translatey(0px)"},
-         },
-         opacityPulse:{
-          "0%": {opacity: 1},
-          "50%": {opacity: 0.2},
-          "100%": {opacity: 1},
-         }
-        },
-        animation:{
-          floatRight:"floatRight 2.5s infinite",
-          opacityPulse:"opacityPulse 2.5s infinite",
-        }
     },
+
   },
   plugins: [],
 }
