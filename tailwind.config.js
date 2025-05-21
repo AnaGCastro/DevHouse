7afce23c6cc0b572f79e6c45c60ba8f2a6e45409
AnaGCastro <testeanateste0@gmail.com>
2025-05-21 14:58:36 -0300

    Personalizando animacoes e keyframes

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,22 @@ module.exports = {
       backgroundImage: {
         "tenis": "url('./src/assets/images/bg.png')",
       },
-   
+      keyframes: {
+        floatRight: {
+          "0%": {tranform: "translatey(0px)"},
+          "50%": {tranform: "translatey(8px)"},
+          "100%": {tranform: "translatey(0px)"},
+         },
+         opacityPulse:{
+          "0%": {opacity: 1},
+          "50%": {opacity: 0.2},
+          "100%": {opacity: 1},
+         }
+        },
+        animation:{
+          floatRight:"floatRight 2.5s infinite",
+          opacityPulse:"opacityPulse 2.5s infinite",
+        }
     },
   },
   plugins: [],
